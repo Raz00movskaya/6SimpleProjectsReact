3a3c8dca56fb122ffe2f479a5058930db8c6e211
Raz00movskaya <darmograyp@yandex.ru>
2022-10-09 23:43:39 +0200

    task1

--- src/App.js
+++ src/App.js
@@ -1,15 +1,25 @@
-import './index.scss';
+import React from 'react';
+import '.index.scss';
 
 function App() {
+
+  const [count, setCount] = React.useState(0);
+
+  const increment = () => {
+    setCount(count + 1)
+  }
+  const decrement = () => {
+    setCount(count - 1)
+  }
   return (
-    <div className="App">
+    <div className ="App">
       <div>
-        <h2>Счетчик:</h2>
-        <h1>0</h1>
-        <button className="minus">- Минус</button>
-        <button className="plus">Плюс +</button>
+        <h2>Счетчик</h2>
+        <h1>{count}</h1>
+        <button onClick={decrement} className="minus">- Минус</button>
+        <button onClick={increment} className="plus">Плюс +</button>
       </div>
-    </div>
+      </div>
   );
 }
 
